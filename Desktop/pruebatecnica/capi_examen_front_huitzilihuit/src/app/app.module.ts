077f24e20c8d1ed6d4427581a90ac1838974160d
hdeleonleal@gmail.com <hdeleonl@sigma-alimentos.com>
2023-04-25 15:06:59 -0600

    finish

--- Desktop/pruebatecnica/capi_examen_front_huitzilihuit/src/app/app.module.ts
+++ Desktop/pruebatecnica/capi_examen_front_huitzilihuit/src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { TablaUsuariosComponent } from './usuarios/tabla-usuarios/tabla-usuarios.component';
+const appRoutes:Routes=[
+  {
+    path:'', component:TablaUsuariosComponent
+  }, 
+    
+  
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes),
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
